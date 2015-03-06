45fb569ee148c4ba6bbb79cb73566d5847dfa97b
Joel Schow <jwschow@ucsd.edu>
2015-03-05 18:16:23 -0800

    final week 9 again

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ var storyData = require('../storyData.json');
 
 exports.view = function(req, res){
 	console.log(storyData);
-	var random_num = Math.random();
+	//var random_num = Math.random();
 
-  if(random_num > 0.5)
-  {
+  //if(random_num > 0.5)
+  //{
     res.render("index", storyData);
-  }
-  else
-  {
-    res.render("index_alternate", storyData)
-  }
+  //}
+  //else
+  //{
+   // res.render("index_alternate", storyData)
+  //}
 
 
 };
